ef2d02309ba569f461b1cd95c9f2d1d2761987f0
contact <contact@techadvancer.com>
2024-11-20 17:09:36 -0600

    fix: removing duplicated initial response

--- src/app/core/pipes/markdown.pipe.ts
+++ src/app/core/pipes/markdown.pipe.ts
@@ -1,13 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { marked } from 'marked';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
     name: 'markdown',
-    standalone: true
+    standalone: true,
 })
 export class MarkdownPipe implements PipeTransform {
-    transform(value: string): string {
+    constructor(private sanitizer: DomSanitizer) { }
+
+    transform(value: string): SafeHtml {
         const markdown = value || '';
-        return marked.parse(markdown) as string;
+        if (!this.containsMarkdown(markdown)) {
+            return markdown;
+        }
+        const html = marked.parse(markdown) as string;
+        return this.sanitizer.bypassSecurityTrustHtml(html);
+    }
+
+    private containsMarkdown(text: string): boolean {
+        return /[*_`#]/.test(text);
     }
 }
